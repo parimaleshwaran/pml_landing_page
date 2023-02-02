b87718fc9f183291b5a7ea3e2128bf860a6e19ce
parimaleshwaran <parimaleshbabu2006@gmail.com>
2023-02-02 17:29:47 +0530

    modified design changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
 			fontFamily: {
 				sans: ["Figtree"],
 			},
+			boxShadow: {
+				mf: "4px 4px 20px 0 rgb(15 15 15 / 10%), 0 0 4px 0 rgb(51 35 77 / 5%)",
+			},
 		},
 	},
 	plugins: [],
